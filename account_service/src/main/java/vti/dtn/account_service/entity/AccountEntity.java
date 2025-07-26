736f84b86c158323cfa0225afd5aa2bb4a7a5574
hungnguyenuet <hungnguyenuet@gmail.com>
2025-07-26 20:00:18 +0700

    Init account-service

---
+++ account_service/src/main/java/vti/dtn/account_service/entity/AccountEntity.java
@@ -0,0 +1,25 @@
+package vti.dtn.account_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "account")
+public class AccountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+}
