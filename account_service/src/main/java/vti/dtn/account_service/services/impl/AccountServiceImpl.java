175cc5efc428fd52c0c83eacb796f01733f09089
hungnguyenuet <hungnguyenuet@gmail.com>
2025-07-26 21:17:48 +0700

    Add department service

---
+++ account_service/src/main/java/vti/dtn/account_service/services/impl/AccountServiceImpl.java
@@ -0,0 +1,27 @@
+package vti.dtn.account_service.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import vti.dtn.account_service.dto.AccountDTO;
+import vti.dtn.account_service.entity.AccountEntity;
+import vti.dtn.account_service.reppository.AccountRepository;
+import vti.dtn.account_service.services.AccountService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+    private final AccountRepository accountRepository;
+
+    @Override
+    public List<AccountDTO> getListAccounts() {
+        List<AccountEntity> accountEntities = accountRepository.findAll();
+        return accountEntities.stream().map(accountEntity -> new AccountDTO(
+                accountEntity.getId(),
+                accountEntity.getUsername(),
+                accountEntity.getFirstName(),
+                accountEntity.getLastName())).toList();
+    }
+
+}
