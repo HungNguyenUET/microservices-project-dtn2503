09c61a5d3ec662709617aef574d0814bd68dad95
hungnguyenuet <hungnguyenuet@gmail.com>
2025-07-29 22:02:10 +0700

    Implement user registration feature with validation and password encoding

---
+++ auth-service/src/main/java/vti/dtn/auth_service/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package vti.dtn.auth_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    private static final String[] WHITE_LIST_URL = {
+            "/api/v1/user/register"
+    };
+
+    @SuppressWarnings("removal")
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+}
