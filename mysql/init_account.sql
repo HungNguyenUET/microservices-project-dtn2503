857e41a651155b4425190c1d938ced1740348930
hungnguyenuet <hungnguyenuet@gmail.com>
2025-10-19 16:56:11 +0700

    Update database configurations and add initialization scripts for account, department, and user databases

---
+++ mysql/init_account.sql
@@ -0,0 +1,32 @@
+-- Drop the database if it already exists
+DROP DATABASE IF EXISTS microservice_account_db;
+-- Create database
+CREATE DATABASE IF NOT EXISTS microservice_account_db;
+USE microservice_account_db;
+
+-- create table: account
+DROP TABLE IF EXISTS account;
+CREATE TABLE account(
+    id						INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username				VARCHAR(50) NOT NULL UNIQUE KEY,
+    firstname				VARCHAR(50),
+    lastname				VARCHAR(50),
+    department_id 			INT UNSIGNED NOT NULL,
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+ -- =============================================
+ -- INSERT DATA
+ -- =============================================
+
+ -- Add data account
+ INSERT INTO account(	username		,	department_id	)
+ VALUES 			(	'dangblack'		,   	'5'			),
+ 					(	'quanganh'		,		'1'			),
+                    (	'vanchien'		,		'1'			),
+                    (	'cocoduongqua'	,		'1'			),
+                    (	'doccocaubai'	,   	'2'			),
+                    (	'khabanh'		,   	'2'			),
+                    (	'huanhoahong'	,   	'2'			),
+                    (	'tungnui'		,   	'8'			),
+                    (	'duongghuu'		,   	'9'			);
